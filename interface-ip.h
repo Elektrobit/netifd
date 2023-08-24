0b2a93aa14d004db039a20487707627b04fe3dbc
Jo-Philipp Wich <jo@mein.io>
2023-08-24 14:43:31 +0200

    interface-ip: allow configuring routes without explicit interface
    Support the configuration of network routes not bound to any specific
    interface. In case such a route is configured, it will be internally
    owned by the loopback interface and have a new DEVROUTE_NODEV flag
    set to inhibit the RTA_OIF attribute when installing the kernel route.
    
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- interface-ip.h
+++ interface-ip.h
@@ -52,8 +52,11 @@ enum device_addr_flags {
 	/* neighbor mac address */
 	DEVNEIGH_MAC		= (1 << 11),
 
+	/* route specifies no device */
+	DEVROUTE_NODEV		= (1 << 12),
+
 	/* advertised mss */
-	DEVROUTE_ADVMSS     = (1 << 12),
+	DEVROUTE_ADVMSS     = (1 << 13),
 };
 
 union if_addr {
